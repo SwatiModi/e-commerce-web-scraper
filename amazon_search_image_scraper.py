9595b88f422ba3b701e43d377d0e214e728e2ea1
Swati Modi <swatimodi1609@gmail.com>
2019-05-15 13:44:42 +0530

    adding header and small changes

--- amazon_search_image_scraper.py
+++ amazon_search_image_scraper.py
@@ -1,6 +1,11 @@
 import requests
 from bs4 import *
 
+headers_std = {
+'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36',
+'Content-Type': 'text/html',
+}
+
 link = input('Search Query URL: ')
 req = requests.get(link)
 soup = BeautifulSoup(req.text, 'lxml')
@@ -18,9 +23,9 @@ k = 1
 pag = 1
 for x in urls:
     print('Pagination : ' + str(pag))
-    req = requests.get(x)
-    soup = BeautifulSoup(req.text, 'lxml')
-    imgs = soup.find_all('img')
+    req = requests.get(x,headers=headers_std).text
+    soup = BeautifulSoup(req, 'lxml')
+    imgs = soup.find_all('img',{'class':'s-image'})
    
     for i in imgs:
         if str(i).find('src') != -1:
